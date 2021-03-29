3a11247b416b1728226e55fea0bf75ad4ba04fcf
sam <samayer12@gmail.com>
2021-03-29 19:17:49 -0400

    Create test file

---
+++ test/com/company/FunctionsTest.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FunctionsTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void isOperator() {
+    }
+
+    @Test
+    void preFix() {
+    }
+
+    @Test
+    void postFix() {
+    }
+
+    @Test
+    void actionPerformed() {
+    }
+}
