f357f32202daa7717d0d72591da48b22898e2b04
sam <samayer12@gmail.com>
2021-03-30 10:30:53 -0400

    Reformat file

--- src/com/company/Functions.java
+++ src/com/company/Functions.java
@@ -15,7 +15,47 @@ public class Functions implements ActionListener {
         return false;
     }
 
-    public static String generatePostfix(String expression) {
+    public static String generatePrefix(String expression){
+        return generateInfixToPrefix(generatePostFixToInfix(expression));
+    }
+
+    public static String generatePreFixToInfix(String expression) {
+        Stack<String> s = new Stack<>();
+
+        // reading from right to left
+        for (int i = expression.length() - 1; i >= 0; i--)
+        {
+            if (isOperator(expression.charAt(i)))
+            {
+                // pop two operands from stack
+                String op1 = s.peek();
+                s.pop();
+                String op2 = s.peek();
+                s.pop();
+
+                // concat the operands and operator
+                String temp = op1 + op2 + expression.charAt(i);
+                s.push(temp);
+            }
+
+            // if symbol is an operand
+            else {
+                s.push(expression.charAt(i) + "");
+            }
+        }
+
+        return s.peek();
+    }
+
+    public static String generateInfixToPrefix(String expression) {
+        return expression;
+    }
+
+    public static String generatePostfix(String expression){
+        return generateInfixToPostfix(generatePreFixToInfix(expression));
+    }
+
+    public static String generatePostFixToInfix(String expression) {
 
         Stack<String> s = new Stack<>();
 
@@ -46,32 +86,9 @@ public class Functions implements ActionListener {
             ans.append(i);
         return ans.toString();
     }
-    public static String generatePrefix(String expression) {
-        Stack<String> s = new Stack<>();
 
-        // reading from right to left
-        for (int i = expression.length() - 1; i >= 0; i--)
-        {
-            if (isOperator(expression.charAt(i)))
-            {
-                // pop two operands from stack
-                String op1 = s.peek();
-                s.pop();
-                String op2 = s.peek();
-                s.pop();
-
-                // concat the operands and operator
-                String temp = op1 + op2 + expression.charAt(i);
-                s.push(temp);
-            }
-
-            // if symbol is an operand
-            else {
-                s.push(expression.charAt(i) + "");
-            }
-        }
-
-        return s.peek();
+    public static String generateInfixToPostfix(String expression) {
+        return expression;
     }
 
     @Override
